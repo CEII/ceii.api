071c81388f36784cd3f5a65144362121ca60319f
wmoralesdev <walterrafael26@gmail.com>
2021-12-01 19:58:44 -0600

    feat: Configured dependency injection for SqlServer

--- src/Ceii.Api.Core/Program.cs
+++ src/Ceii.Api.Core/Program.cs
@@ -1,8 +1,26 @@
 // MinimalApi -> net6.0
+using Ceii.Api.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Add DbContext to builder
+
+/// <summary>
+/// Options sets connection string from env variables, then sets assembly for project that should contain migrations
+/// and contains DbContext class
+/// </summary>
+builder.Services.AddDbContext<CeiiDbContext>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("CeiiDb"),
+        b => b.MigrationsAssembly("Ceii.Api.Data")
+));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
